0bdcb1667772cded5d436a58b3a6f3d123585323
teacher <net94.teacher@gmail.com>
2020-04-02 10:51:43 +0900

    one to many complete

---
+++ src/main/java/mybatis_study_teacher/mappers/TutorMapperImpl.java
@@ -0,0 +1,29 @@
+package mybatis_study_teacher.mappers;
+
+import org.apache.ibatis.session.SqlSession;
+
+import mybatis_study_teacher.dto.Tutor;
+
+public class TutorMapperImpl implements TutorMapper {
+	private static final TutorMapperImpl instance = new TutorMapperImpl();
+
+	private final String namespace = "mybatis_study_teacher.mappers.TutorMapper";
+	private SqlSession sqlSession;
+
+	private TutorMapperImpl() {}
+
+	public static TutorMapperImpl getInstance() {
+		return instance;
+	}
+
+	public void setSqlSession(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+
+	
+	@Override
+	public Tutor selectTutorByTutorId(Tutor tutor) {
+		return sqlSession.selectOne(namespace + ".selectTutorByTutorId", tutor);
+	}
+
+}
